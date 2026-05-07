1f2cac889894038a82cef9bd52bf8496948f3fab
Luan Oliveira <lhenrique.oliv@gmail.com>
2026-05-07 12:49:32 +0000

    att

---
+++ src/topico081.php
@@ -0,0 +1,14 @@
+<?php
+
+$a=30;
+$a=10;
+$a=5;
+echo "<br> $a";
+$arr=[30,10,5];
+$arr[0]+=5;
+echo "<br>arr[0] = ".$arr[0];
+echo "<br>arr[0] = ".$arr[1];
+echo "<br>arr[0] = ".$arr[2];
+$uf=["SP", "RJ", "MG", "ES"];
+echo "<br>".$uf[2];
+?>
